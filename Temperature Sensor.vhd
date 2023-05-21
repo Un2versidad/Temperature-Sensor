eccfb66a9fef6a0cfb0f743bc4528b15146c5ba1
Nova <69091361+qzxtu@users.noreply.github.com>
2023-05-21 13:41:29 -0500

    Update Temperature Sensor.vhd

--- Temperature Sensor.vhd
+++ Temperature Sensor.vhd
@@ -4,22 +4,22 @@ use ieee.numeric_std.all;
 
 entity TemperatureSensor is
   port (
-    clk      : in  std_logic;
-    reset    : in  std_logic;
+    clk         : in  std_logic;
+    reset       : in  std_logic;
     temperature : out std_logic_vector(7 downto 0)
   );
 end TemperatureSensor;
 
 architecture Behavioral of TemperatureSensor is
-  signal temperature_reg   : std_logic_vector(7 downto 0);
+  signal temperature_reg : std_logic_vector(7 downto 0);
 begin
   process(clk)
   begin
     if rising_edge(clk) then
       if reset = '1' then
-        temperature_reg <= (others => '0');  -- Inicializar la temperatura en cero
+        temperature_reg <= (others => '0');  -- Initialize temperature to zero
       else
-        -- Simulación de lectura de temperatura
+        -- Simulation of temperature reading
         temperature_reg <= std_logic_vector(to_unsigned(simulateTemperature(), 8));
       end if;
     end if;
@@ -28,16 +28,15 @@ begin
   temperature <= temperature_reg;
 end Behavioral;
 
--- Función de simulación de temperatura
+-- Function for temperature simulation
 function simulateTemperature return natural is
-  variable temp : natural := 25;  -- Temperatura inicial de simulación
+  variable temp : natural := 25;  -- Initial simulation temperature
 begin
-  -- Código de simulación de temperatura
-  -- Aquí puedes implementar la lógica para generar una temperatura simulada
-  -- Puede ser un cálculo aleatorio o basado en algún modelo o algoritmo específico
+  -- Simulation code for generating temperature values
+  -- Customize this function to implement your desired temperature simulation logic
 
-  -- Ejemplo: Simulación de temperatura aleatoria entre 20 y 30 grados Celsius
-  temp := temp + natural(random_number(10) - 5);  -- Variación aleatoria entre -5 y 5 grados
+  -- Example: Simulating random temperature between 20 and 30 degrees Celsius
+  temp := temp + natural(random_number(10) - 5);  -- Random variation between -5 and +5 degrees
   if temp < 20 then
     temp := 20;
   elsif temp > 30 then
